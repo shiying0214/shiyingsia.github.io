82a66a5582948fce59b433ed3e1822af277992bb
shiying0214 <126548618+shiying0214@users.noreply.github.com>
2023-04-26 20:53:30 +0800

    initial commit

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+
+
+function printInput() {
+let formInput = document.getElementById('Name')
+let formInput2 = document.getElementById('Email')
+let formInput3 = document.getElementById('Message')
+
+alert('Name: '+formInput.value+ '\n' +'Email: '+formInput2.value+ '\n' +'Message: '+formInput3.value)
+console.log('Name: '+formInput.value+ '\n' +'Email: '+formInput2.value+ '\n' +'Message: '+formInput3.value)
+   
+}  
